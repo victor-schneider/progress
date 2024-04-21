9ded8d147f060d713803987a57fd98dec56df79d
victor-schneider <victor.8121966@edu.univali.br>
2024-04-21 19:22:42 -0300

    Adicionada a função de pirâmede de A

--- exercicios/main.cpp
+++ exercicios/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello world!";
+int main() {
+  int num;
+
+  
+  cout << "Quantas linhas de pirâmede você quer?: " << endl;
+  cin >> num;
+  
+  for(int i = 0; i < num; i++){
+    string A;
+    for(int x = 0; x < i; x++){
+      A += "A";
+    }
+    cout << A << endl;
+  }
 }
